99b0819299522436e4eced945b1bf022e5c7a1a9
hugodavilat <hugo.davilat@gmail.com>
2021-06-29 00:06:50 -0300

    adding entropy
    shannon entropy as curiosity measure

--- Movie_recommenders/predict.py
+++ Movie_recommenders/predict.py
@@ -5,6 +5,18 @@ import numpy as np
 
 from collections import defaultdict
 
+def shannon_entropy(sequence):
+    count_dict = defaultdict(int)
+    total = len(sequence)
+    for xj in sequence:
+        count_dict[xj] += 1
+    entropy = 0
+    for xj in count_dict.keys():
+        p_xj = count_dict[xj]/total
+        h_xj = np.log2(1/p_xj)
+        entropy += p_xj*h_xj
+    return entropy
+
 def entropy_kontoyiannis(sequence):
     if not sequence:
         return 0.0    
@@ -37,9 +49,10 @@ def pred(x):
 # Used data structures.
 genre_per_itm = {}
 user_views_genre = defaultdict(list)
+count_genres = defaultdict(int)
 
 # Populate genre_per_itm.
-with open('data/ml-100k/u.item', encoding = "ISO-8859-1") as itm_file:
+with open('../data/ml-100k/u.item', encoding = "ISO-8859-1") as itm_file:
     for itm in itm_file.readlines():
         itm_genres = []
         chars = itm.split('|')
@@ -48,35 +61,51 @@ with open('data/ml-100k/u.item', encoding = "ISO-8859-1") as itm_file:
             if chars[i]=='1':
                 itm_genres.append(i-5)
         genre_per_itm[iid] = itm_genres
+        for genre in itm_genres:
+            count_genres[genre] += 1
+
+print(count_genres)
+exit(0)
 
 # Populate user_view_by_genre.
-with open('data/ml-100k/train.csv') as train_file:
+with open('../data/ml-100k/train.csv') as train_file:
     for row in csv.reader(train_file, delimiter='\t'):
         for genre in genre_per_itm[int(row[1])]:
-            user_views_genre[int(row[0])].append(str(genre))
+            user_views_genre[int(row[0])].append(genre)
 
 # Create uid x pred str to save.
 out_str = ""
 user_prevs = []
+user_entropies = []
 max_prev, min_prev = -1, 1
 for uid in range(1, 944):
-    prev = pred(user_views_genre[uid])
-    if prev>0.3:
-        user_prevs.append((uid, prev))
-        min_prev = min(min_prev, prev)
-        max_prev = max(max_prev, prev)
-    else:
-        user_prevs.append((uid, 0.5))
+    entropy = shannon_entropy(user_views_genre[uid])
+    user_entropies.append(entropy)
+min_entropy = min(user_entropies)
+max_entropy = max(user_entropies)
+    # prev = pred(user_views_genre[uid])
+    # if prev>0.3:
+        # user_prevs.append((uid, prev))
+        # min_prev = min(min_prev, prev)
+        # max_prev = max(max_prev, prev)
+    # else:
+        # user_prevs.append((uid, 0.5))
+
+# Normalize entropies.
+for i in range(len(user_entropies)):
+    # e = user_entropies[i]
+    # e2 = (e-min_entropy)/(max_entropy-min_entropy)
+    out_str += "{}\t{}\n".format(i+1, user_entropies[i])
 
 # Normalize preds.
-for i in range(len(user_prevs)):
-    u = user_prevs[i][0]
-    p = user_prevs[i][1]
-    p2 = (p-min_prev)/(max_prev-min_prev)
-    user_prevs[i] = (u, p2)
-    out_str += "{}\t{}\n".format(u, p2)
+# for i in range(len(user_prevs)):
+#     u = user_prevs[i][0]
+#     p = user_prevs[i][1]
+#     p2 = (p-min_prev)/(max_prev-min_prev)
+#     user_prevs[i] = (u, p2)
+#     out_str += "{}\t{}\n".format(u, p2)
 
 print(out_str)
 
-# with open("data/digested/ml-100k/user_vs_user_pred.csv", 'w') as pred_file:
-#     pred_file.write(out_str)
+with open("../data/digested/ml-100k/user_vs_user_entropy.csv", 'w') as pred_file:
+    pred_file.write(out_str)
