5700fbce83b029cc27f4257556e3b0ed6ce7c03d
hugodavilat <hugo.davilat@gmail.com>
2022-01-21 00:19:49 -0300

    new charts
    new charts

--- explanatory_framework/features_calculator/user_features.py
+++ explanatory_framework/features_calculator/user_features.py
@@ -7,9 +7,12 @@ import pandas as pd
 from collections import defaultdict
 from scipy.stats import variation, tstd, entropy
 
+METRICS = ["gini", "variation", "num_ratings", "rating_avg",
+           "rating_std", "phi", "avg_phi", "entropy", "entropy_of_consumed_sum",
+           "entropy_of_consumed_avg", "proportion_long_tailors"]
 
 class FeatureCalculator:
-    def __init__(self, id, urm, itm_phis, usr_phis, is_user=True):
+    def __init__(self, id, urm, itm_phis, usr_phis, itm_H, usr_H, item_long_tails, user_long_tails, is_user=True):
         self.urm = urm
         self.id = id
         self.is_user = is_user
@@ -17,14 +20,22 @@ class FeatureCalculator:
         self.num_ratings_urm = len(urm.nonzero()[0])
         self.num_itens = urm.getrow(0).shape[1]
         self.num_users = urm.getcol(0).shape[0]
+        self.non_zeros = None
+        self.long_tailors = None,
         if is_user:
-            for j in urm.getrow(id).nonzero()[1]:
+            self.long_tailors = user_long_tails
+            self.non_zeros = urm.getrow(id).nonzero()[1]
+            for j in self.non_zeros:
                 self.ratings.append(urm[self.id, j])
         else:
-            for i in urm.getcol(id).nonzero()[0]:
+            self.long_tailors = item_long_tails
+            self.non_zeros = urm.getcol(id).nonzero()[0]
+            for i in self.non_zeros:
                 self.ratings.append(urm[i, self.id])
         self.item_phis = itm_phis
         self.user_phis = usr_phis
+        self.itm_H = itm_H
+        self.usr_H = usr_H
     
     def _gini(self, x):
         # Mean absolute difference
@@ -55,11 +66,30 @@ class FeatureCalculator:
             phi += self.user_phis[usr]
         return phi
 
-    def entropy(self):
-        pd_series = pd.Series(self.ratings)
-        counts = pd_series.value_counts()
-        return entropy(counts)
+    def entropy_consumed(self):
+        ent = 0
+        if self.is_user:
+            row = self.urm.getrow(self.id).nonzero()[1]
+            for itm in row:
+                ent += self.itm_H[itm]
+            return ent
+        # elif is item:
+        row = self.urm.getcol(self.id).nonzero()[0]
+        for usr in row:
+            ent += self.user_phis[usr]
+        return ent
     
+    def long_tail(self):
+        num_tails = 0
+        total = len(self.non_zeros)
+        for i in self.non_zeros:
+            if i in self.long_tailors:
+                num_tails += 1
+        if total != 0:
+            return num_tails/total
+        else:
+            return 0
+
     def get_metrics(self):
         phi = self.phi_id()
         avg = 0
@@ -67,10 +97,19 @@ class FeatureCalculator:
         std = tstd(self.ratings)
         gini = self.gini()
         var = variation(self.ratings)
+        ent = 0
+        long_tail = self.long_tail()
+        avg_ent_consumed = 0
+        if self.is_user:
+            ent = self.usr_H[self.id]
+        else:
+            ent = self.itm_H[self.id]
+        ent_consumed = self.entropy_consumed()
 
         if len(self.ratings) != 0:
             avg = sum(self.ratings)/len(self.ratings)
             avg_phi = phi/len(self.ratings)
+            avg_ent_consumed = ent_consumed/len(self.ratings)
         
         if np.isnan(std):
             std = 0
@@ -87,22 +126,25 @@ class FeatureCalculator:
             "variation": var,
             "num_ratings": len(self.ratings),
             "rating_avg": avg,
-            "rating_std_dev": std,
+            "rating_std": std,
             "phi": phi,
             "avg_phi": avg_phi,
-            "entropy": entropy(),
+            "entropy": ent,
+            "entropy_of_consumed_sum": ent_consumed,
+            "entropy_of_consumed_avg": avg_ent_consumed,
+            "proportion_long_tailors": long_tail
         }
         return metrics_map
 
 
 
 class UserFeatures(FeatureCalculator):
-    def __init__(self, uid, urm, itm_phis, usr_phis):
-        super().__init__(uid, urm, itm_phis, usr_phis, True)
+    def __init__(self, uid, urm, itm_phis, usr_phis, itm_H, usr_H, item_long_tails, user_long_tails):
+        super().__init__(uid, urm, itm_phis, usr_phis, itm_H, usr_H, item_long_tails, user_long_tails, True)
 
 class ItemFeatures(FeatureCalculator):
-    def __init__(self, iid, urm, itm_phis, usr_phis):
-        super().__init__(iid, urm, itm_phis, usr_phis, False)
+    def __init__(self, iid, urm, itm_phis, usr_phis, itm_H, usr_H, item_long_tails, user_long_tails):
+        super().__init__(iid, urm, itm_phis, usr_phis, itm_H, usr_H, item_long_tails, user_long_tails, False)
 
 def get_phis(urm):
     num_ratings_urm = len(urm.nonzero()[0])
@@ -117,30 +159,82 @@ def get_phis(urm):
     
     return item_phis, user_phis
 
+def get_entropies(urm):
+    def entropy_(x):
+        pd_series = pd.Series(x)
+        counts = pd_series.value_counts()
+        return entropy(counts)
+    
+    num_itens = urm.getrow(0).shape[1]
+    num_users = urm.getcol(0).shape[0]
+    item_entropies, user_entropies = defaultdict(float), defaultdict(float)  
+
+    for itm in range(num_itens):
+        x = []
+        for usr in urm.getcol(itm).nonzero()[0]:
+            x.append(urm[usr, itm])
+        item_entropies[itm] = entropy_(x)
+    for usr in range(num_users):
+        x = []
+        for itm in urm.getrow(usr).nonzero()[1]:
+            x.append(urm[usr, itm])
+        user_entropies[usr] = entropy_(x)
+
+    return item_entropies, user_entropies
+
+def get_long_tailors(urm):
+    (U, I) = urm.get_shape()
+    R = urm.count_nonzero()
+    distribution = []
+    for j in range(I):
+        distribution.append((urm.getcol(j).count_nonzero(), j))
+    distribution.sort(key=lambda x: x[0])
+    item_long_tails = set()
+    views, i = 0, 0
+    while views/R < 0.2:
+        item_long_tails.add(distribution[i][1])
+        views += distribution[i][0]
+        i += 1
+    
+    distribution = []
+    for i in range(U):
+        distribution.append((urm.getrow(i).count_nonzero(), i))
+    distribution.sort(key=lambda x: x[0])
+    user_long_tails = set()
+    views, i = 0, 0
+    while views/R < 0.2:
+        user_long_tails.add(distribution[i][1])
+        views += distribution[i][0]
+        i += 1
+    return item_long_tails, user_long_tails
+
 
 # DS = "ml-100k"
 DS = "ml-1M"
 urm = pickle.load(open(f"../datasets/{DS}/urm.pk", 'rb'))
 
 item_phis, user_phis = get_phis(urm)
+item_entropies, user_entropies = get_entropies(urm)
+item_long_tails, user_long_tails = get_long_tailors(urm)
 
-num_users = urm.getcol(0).shape[0]
-usr_out_str = f"uid, gini, variation, num_ratings, rating_avg, rating_std, phi, avg_phi\n"
-for usr in range(num_users):
-    uf = UserFeatures(usr, urm, item_phis, user_phis)
-    m = uf.get_metrics()
-    usr_out_str += f"{m['id']}, {m['gini']:.5f}, {m['variation']:.5f}, {m['num_ratings']:.5f}, {m['rating_avg']:.5f}, {m['rating_std_dev']:.5f}, {m['phi']:.5f}, {m['avg_phi']:.5f}\n"
+# num_users = urm.getcol(0).shape[0]
+# usr_out_str = "uid, " + ", ".join(METRICS) + '\n'
+# for usr in range(num_users):
+#     uf = UserFeatures(usr, urm, item_phis, user_phis, item_entropies, user_entropies, item_long_tails, user_long_tails)
+#     m = uf.get_metrics()
+#     usr_out_str += f'{m["id"]}, ' + ", ".join([f'{m[metric]:.5f}' for metric in METRICS]) + "\n"
 
-with open(f"../datasets/{DS}/user_metrics.csv", 'w') as metrics_file:
-    metrics_file.write(usr_out_str)
+# # print(usr_out_str)
+# with open(f"../datasets/{DS}/user_metrics.csv", 'w') as metrics_file:
+#     metrics_file.write(usr_out_str)
 
 num_itens = urm.getrow(0).shape[1]
-itm_out_str = f"iid, gini, variation, num_ratings, rating_avg, rating_std, phi, avg_phi\n"
+itm_out_str = "iid, " + ", ".join(METRICS) + '\n'
 for itm in range(num_itens):
-    itmf = ItemFeatures(itm, urm, item_phis, user_phis)
-    m = itmf.get_metrics()
-    # print(m)
-    itm_out_str += f"{m['id']}, {m['gini']:.5f}, {m['variation']:.5f}, {m['num_ratings']:.5f}, {m['rating_avg']:.5f}, {m['rating_std_dev']:.5f}, {m['phi']:.5f}, {m['avg_phi']:.5f}\n"
+    uf = ItemFeatures(itm, urm, item_phis, user_phis, item_entropies, user_entropies, item_long_tails, user_long_tails)
+    m = uf.get_metrics()
+    itm_out_str += f'{m["id"]}, ' + ", ".join([f'{m[metric]:.5f}' for metric in METRICS]) + "\n"
 
+# print(itm_out_str)
 with open(f"../datasets/{DS}/item_metrics.csv", 'w') as metrics_file:
     metrics_file.write(itm_out_str)
